3e357b77adebef18808f1af6949885d3f3bf6652
Ziyanda Dyani <ziyandadyani17@gmail.com>
2023-04-13 11:46:35 +0200

    fixed IWA 9 challenge 1 code to make it log correct balance

--- script.js
+++ script.js
@@ -30,8 +30,11 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
+const taxAsDecimal = parseInt(tax[913].substring(0,tax[913].length-1)) / 100
+const startingAfterTax = salary - salary*taxAsDecimal
+const type = `${size}-${lodging}`
+
+//console.log(type)
+const balance = startingAfterTax - expenses.transport - expenses.food - rent[`${type}`]
+
+console.log(balance.toFixed(2))
